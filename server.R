282c9a4c34bb3d2f4d44df5fd966fc019c668dc9
Erica Leh <erica.leh@yale.edu>
2015-06-06 00:31:29 -0400

    expanded dataset, new legend

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(lpSolveAPI)
 library(shiny)
 library(dplyr)
 
-SNAP2 <- read.csv("./data/SNAP2.csv")
+SNAP <- read.csv("./data/SNAP.csv")
 nutrients <- read.csv("./data/nutrientByDemoT.csv")
 
   function(input, output, session) {
@@ -15,23 +15,58 @@ nutrients <- read.csv("./data/nutrientByDemoT.csv")
   # set up optimization eq maximizing protein given constraints
   maxProt <- lp("min",
                 switch(input$select, 
-                       "Cost" = SNAP2$costPerServ,
-                       "Calories" = SNAP2$calsPerServ,
-                       "Fat" = SNAP2$fatPerServ,
-                       "Sugar" = SNAP2$sugarPerServ,
-                       "Sodium" = SNAP2$sodiumPerServ),
-                rbind(SNAP2$protPerServ, SNAP2$protPerServ, SNAP2$fatPerServ, SNAP2$fatPerServ, SNAP2$costPerServ, SNAP2$costPerServ,
-                      SNAP2$sodiumPerServ, SNAP2$sodiumPerServ, SNAP2$fiberPerServ, SNAP2$fiberPerServ,
-                      SNAP2$sugarPerServ, SNAP2$sugarPerServ, SNAP2$calsPerServ, SNAP2$calsPerServ,
-                      SNAP2$fruit, SNAP2$vegs, SNAP2$grains, SNAP2$grains, SNAP2$meatProtein, SNAP2$dairy, SNAP2$V1, SNAP2$V2, SNAP2$V3, SNAP2$V4, SNAP2$V5, SNAP2$V6, SNAP2$V7, SNAP2$V8, SNAP2$V9, SNAP2$V10, SNAP2$V11, SNAP2$V12, SNAP2$V13, SNAP2$V14, SNAP2$V15, SNAP2$V16, SNAP2$V17, SNAP2$V18, SNAP2$V19, SNAP2$V20, SNAP2$V21, SNAP2$V22, SNAP2$V23, SNAP2$V24, SNAP2$V25, SNAP2$V26, SNAP2$V27, SNAP2$V28, SNAP2$V29, SNAP2$V30, SNAP2$V31, SNAP2$V32, SNAP2$V33, SNAP2$V34, SNAP2$V35, SNAP2$V36, SNAP2$V37, SNAP2$V38, SNAP2$V39, SNAP2$V40, SNAP2$V41, SNAP2$V42, SNAP2$V43, SNAP2$V44, SNAP2$V45, SNAP2$V46, SNAP2$V47, SNAP2$V48, SNAP2$V49, SNAP2$V50, SNAP2$V51, SNAP2$V52, SNAP2$V53, SNAP2$V54, SNAP2$V55, SNAP2$V56, SNAP2$V57, SNAP2$V58, SNAP2$V59, SNAP2$V60, SNAP2$V61, SNAP2$V62, SNAP2$V63, SNAP2$V64, SNAP2$V65, SNAP2$V66, SNAP2$V67, SNAP2$V68, SNAP2$V69, SNAP2$V70, SNAP2$V71),
-                c(">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", ">=", ">=", "<=", ">=", ">=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<="),
+                       "Cost" = SNAP$costPerServ,
+                       "Calories" = SNAP$calsPerServ,
+                       "Fat" = SNAP$fatPerServ,
+                       "Sugar" = SNAP$sugarPerServ,
+                       "Sodium" = SNAP$sodiumPerServ),
+                rbind(SNAP$protPerServ, SNAP$protPerServ, SNAP$fatPerServ, SNAP$fatPerServ, SNAP$costPerServ, SNAP$costPerServ,
+                      SNAP$sodiumPerServ, SNAP$sodiumPerServ, SNAP$fiberPerServ, SNAP$fiberPerServ,
+                      SNAP$sugarPerServ, SNAP$sugarPerServ, SNAP$calsPerServ, SNAP$calsPerServ,
+                      SNAP$fruit, SNAP$vegs, SNAP$grains, SNAP$grains, SNAP$meatProtein, SNAP$dairy,
+                      SNAP$X1, SNAP$X2, SNAP$X3, SNAP$X4, SNAP$X5, SNAP$X6, SNAP$X7, SNAP$V8, SNAP$V9,
+                      SNAP$X10, SNAP$X11, SNAP$X12, SNAP$X13, SNAP$X14, SNAP$X15, SNAP$X16, SNAP$X17, SNAP$X18, SNAP$X19,
+                      SNAP$X20, SNAP$X21, SNAP$X22, SNAP$X23, SNAP$X24, SNAP$X25, SNAP$X26, SNAP$X27, SNAP$X28, SNAP$X29,
+                      SNAP$X30, SNAP$X31, SNAP$X32, SNAP$X33, SNAP$X34, SNAP$X35, SNAP$X36, SNAP$X37, SNAP$X38, SNAP$X39,
+                      SNAP$X40, SNAP$X41, SNAP$X42, SNAP$X43, SNAP$X44, SNAP$X45, SNAP$X46, SNAP$X47, SNAP$X48, SNAP$X49,
+                      SNAP$X50, SNAP$X51, SNAP$X52, SNAP$X53, SNAP$X54, SNAP$X55, SNAP$X56, SNAP$X57, SNAP$X58, SNAP$X59,
+                      SNAP$X60, SNAP$X61, SNAP$X62, SNAP$X63, SNAP$X64, SNAP$X65, SNAP$X66, SNAP$X67, SNAP$X68, SNAP$X69,
+                      SNAP$X70, SNAP$X71, SNAP$X72, SNAP$X73, SNAP$X74, SNAP$X75, SNAP$X76, SNAP$X77, SNAP$X78, SNAP$X79,
+                      SNAP$X80, SNAP$X81, SNAP$X82, SNAP$X83, SNAP$X84, SNAP$X85, SNAP$X86, SNAP$X87, SNAP$X88, SNAP$X89,
+                      SNAP$X90, SNAP$X91, SNAP$X92, SNAP$X93, SNAP$X94, SNAP$X95, SNAP$X96, SNAP$X97, SNAP$X98, SNAP$X99,
+                      SNAP$X100, SNAP$X101, SNAP$X102, SNAP$X103, SNAP$X104, SNAP$X105, SNAP$X106, SNAP$X107, SNAP$X108, SNAP$X109,
+                      SNAP$X110, SNAP$X111, SNAP$X112, SNAP$X113, SNAP$X114, SNAP$X115, SNAP$X116, SNAP$X117, SNAP$X118, SNAP$X119,
+                      SNAP$X120, SNAP$X121, SNAP$X122, SNAP$X123, SNAP$X124, SNAP$X125, SNAP$X126, SNAP$X127, SNAP$X128, SNAP$X129,
+                      SNAP$X130, SNAP$X131, SNAP$X132, SNAP$X133, SNAP$X134, SNAP$X135),
+                c(">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", "<=", ">=", ">=", ">=", "<=", ">=", ">=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=",
+                  "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<=", "<="),
                 c(input$prot[1]*7, input$prot[2]*7, input$fat[1]*7, input$fat[2]*7, input$budget[1], input$budget[2],
                   input$sodium[1]*7, input$sodium[2]*7, input$fiber[1]*7, input$fiber[2]*7,
-                  input$sugar[1]*7, input$sugar[2]*7, input$cals[1]*7, input$cals[2]*7, 16, 28, 9, 25, 6.4, 24, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv))
+                  input$sugar[1]*7, input$sugar[2]*7, input$cals[1]*7, input$cals[2]*7, 16, 28, 9, 25, 6.4, 24,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv, input$serv,
+                  input$serv, input$serv, input$serv, input$serv, input$serv))
     
   # drop any variables that have a 0 coefficient in the model
-  x <- data.frame(SNAP2$food, SNAP2$foodGroup, SNAP2$protPerServ, maxProt$solution)
-  x <- droplevels(x[!(x$maxProt.solution == 0),])
+  x <- data.frame(SNAP$food, SNAP$foodGroup, SNAP$protPerServ, maxProt$solution, SNAP$ozPerServ)
+  x <- droplevels(x[!(x$maxProt.solution < 1),])
   })
   
   output$textRecom <- renderText({ 
@@ -46,9 +81,9 @@ nutrients <- read.csv("./data/nutrientByDemoT.csv")
   
   # graph of recommended servings of food based on optim (lp) eq
   x %>%
-    ggvis(~SNAP2.food, ~maxProt.solution, fill = ~SNAP2.foodGroup) %>%
+    ggvis(~SNAP.food, ~maxProt.solution, fill = ~SNAP.foodGroup, stroke = ~SNAP.ozPerServ, strokeWidth := "2") %>%
     layer_points(size := "400", opacity := "0.8") %>%
-    add_tooltip(function(x){paste0("Food: ", x$SNAP2.food, "<br>", "Servings: ",
+    add_tooltip(function(x){paste0("Food: ", x$SNAP.food, "<br>", "Servings: ",
         signif(x$maxProt.solution, 2), "<br>")}, "hover") %>%
     add_axis("x", title = "", ticks = 14,
              properties = axis_props(
@@ -63,14 +98,15 @@ nutrients <- read.csv("./data/nutrientByDemoT.csv")
                axis = list(stroke = "#333", strokeWidth = 1.5)
              )
     ) %>%
-    add_axis("x", orient = "top", ticks = 0, title = "Recommended Servings of
-             Food Given Nutritional Constraints",
+    add_axis("x", orient = "top", ticks = 0, title = "What Foods Should I Buy?",
              properties = axis_props(
                axis = list(stroke = "white"),
                title = list(fontSize = 16),
                labels = list(fontSize = 0))) %>%
     add_axis("y", title = "Servings Per Week") %>%
+    add_relative_scales() %>%
     add_legend("fill", title = "Food Groups") %>%
-    set_options(width = "auto", height = "auto", resizable=FALSE) %>%
+    add_legend("stroke", title = "Oz. Per Serving", properties = legend_props(legend = list(y = 120))) %>%
+    set_options(width = "auto", height = "auto", resizable=FALSE, duration = 0) %>%
     bind_shiny("plot1", "plot_ui")
 }
--- ui.R
+++ ui.R
@@ -13,61 +13,65 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                         font-size: 100; font-family: 'Lobster', cursive; font-weight:500;
                         line-height: 1.1;")),
   sidebarPanel(
-    numericInput("num", label = h5("Enter your household monthly SNAP allotment (in $) to receive
+    numericInput("num", label = h4("Enter your household monthly SNAP allotment (in $) to receive
                  a recommended weekly expenditure on groceries:"), value = 250),
-    h5(textOutput("textRecom")),
+    h4(textOutput("textRecom")),
     sliderInput("budget", 
                 label = "",
                 min = 0, max = 200, value = c(50, 100)), uiOutput("plot_ui"),
     sliderInput("cals", 
-                label = h5("What would you like your daily caloric intake to be?:"),
+                label = h4("What would you like your daily caloric intake to be?:"),
                 min = 0, max = 4000, value = c(2000, 3200)),
     sliderInput("fat", 
-                label = h5("What would you like your daily fat intake (in g) to be?:"),
+                label = h4("What would you like your daily fat intake (in g) to be?:"),
                 min = 0, max = 80, value = c(25, 35)),
     sliderInput("prot", 
-                label = h5("What would you like your daily protein intake (in g) to be?:"),
+                label = h4("What would you like your daily protein intake (in g) to be?:"),
                 min = 0, max = 100, value = c(52, 100)),
     sliderInput("sugar", 
-                label = h5("What would you like your daily sugar intake (in g) to be?:"),
+                label = h4("What would you like your daily sugar intake (in g) to be?:"),
                 min = 0, max = 100, value = c(0, 37.5)),
     sliderInput("fiber", 
-                label = h5("What would you like your daily fiber intake (in g) to be?:"),
+                label = h4("What would you like your daily fiber intake (in g) to be?:"),
                 min = 0, max = 100, value = c(31, 100)),
     sliderInput("sodium", 
-                label = h5("What would you like your daily sodium intake (in mg) to be?:"),
+                label = h4("What would you like your daily sodium intake (in mg) to be?:"),
                 min = 0, max = 4000, value = c(1500, 2300)),
     sliderInput("serv", 
-                label = h5("What is the maximum # of servings of a single food you would
+                label = h4("What is the maximum # of servings of a single food you would
                 be willing to eat per week?:"),
-                min = 0, max = 50, value = 10)
+                min = 0, max = 50, value = 8)
   ),
     mainPanel(
       navbarPage("",
         tabPanel("Interactive Graph",
-                 selectInput("select", label = "I am primarily trying to minimize:",
+                 selectInput("select", label = h4("I am primarily trying to minimize:"),
                     choices = list("Cost", "Calories", "Fat", "Sugar", "Sodium"),
                     selected = "Cost",),
-                 br(),
-                 div(style = "width:800px; height:600px;",
-                     ggvisOutput("plot1")
+                 h4("Hover over points to find out how many servings of each food you should eat in one week.
+                    The outer circle indicates how many ounces are in a serving, which often corresponds to how
+                    filling an item will be. A darker outer circle indicates fewer ounces per serving, or a
+                    less filling serving size.", style = "color:#006699;"),
+                 div(ggvisOutput("plot1")
                  )),
-        
         tabPanel("Nutritional Recommendations",
                  fluidRow(
                    column(10,
-                 h5("The default values to the left are the nutritional guidelines that the USDA and AHA recommend the average 14-18 year old male
+                 h4("The default values to the left are the nutritional guidelines that the USDA and AHA recommend the average 14-18 year old male
                   consume. The graph is constrained by their further recommendations--that Americans
                   consume at least 16 oz fruit, 28 oz vegetables, 9 oz grain,
                   and 24 oz dairy per week. The chart in the 'Interactive Graph' tab allows you
-                  to optimize your diet according to your budget. Hover over points for exact serving
-                  sizes.", style = "color:black;"),
-                 br(),
-                 h5("The table below provides recommended nutritional values based on
+                  to optimize your diet according to your budget. All foods and prices are based off of a 
+                  Key Foods supermarket in Brooklyn, NY.", style = "color:black;"),
+                 h4("The table below provides recommended nutritional values based on
                   age and sex. The calorie range's lower bound is for the most
                   sedentary individuals, while the upper bound is for the most
                   active individuals. It reflects the number of calories to consume
-                  if one intends to remain at a constant weight.", style = "color:black;"),
+                  if one plans to remain at a constant weight.", style = "color:black;"),
+                 h4("To figure out what recipes you can make with your weekly combination
+                    of foods, head on over to",
+                    a("SuperCook.", href = "http://www.supercook.com/#/recipes/All%2520recipes"),
+                    style = "color:black;"),
                  br(),
                  dataTableOutput('nutrients'),
                  tags$head(tags$style("#nutrients table {background:lightpink; color:black;}", type="text/css"))
@@ -77,14 +81,14 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                  column(10,
                  h3("About SNAP",
                     style = "color:#66CCFF; font-family: 'Lobster', cursive; font-weight:500;"),
-                 h5("The Supplemental Nutrition Assistance Program (SNAP) provides a contribution
+                 h4("The Supplemental Nutrition Assistance Program (SNAP) provides a contribution
                     towards food for in-need households. The average SNAP recipient receives $1.39
                     per meal. Nine out of ten Americans receiving SNAP benefits live with children,
                     elderly, or disabled persons, and forty percent live in households below half
                     the poverty line."),
                  h3("Do I Qualify?",
                     style = "color:#66CCFF; font-family: 'Lobster', cursive; font-weight:500;"),
-                 h5("If you are unsure as to whether you qualify, click",
+                 h4("If you are unsure as to whether you qualify, click",
                     a("here.", href = "http://www.snap-step1.usda.gov/fns/"),
                   "If you qualify, you will have to fill out an application and submit it to your
                   local SNAP office. Applications can be found",
@@ -92,17 +96,17 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                   "Call 1-800-221-5689 for more information."),
                  h3("Why participate?",
                     style = "color:#66CCFF; font-family: 'Lobster', cursive; font-weight:500;"),
-                 h5("The Supplemental Nutrition Assistance Program (SNAP) provides a contribution
+                 h4("The Supplemental Nutrition Assistance Program (SNAP) provides a contribution
                   towards food for in-need households. It allows families with unemployed adults
                   to focus on finding work, rather than stressing about putting food on the table.
                   Participating in SNAP is correlated with a higher level of employment:"),
                  div(img(src = "employment.png"), style = "text-align: center;"),
-                 h5("Additionally, SNAP almost completely covers the benefits for which all
+                 h4("Additionally, SNAP almost completely covers the benefits for which all
                     eligible Americans qualify. As such, participation rates are high across
                     the nation. However, the elderly and working-poor are still underserved."),
                  div(img(src = "eligibility.png"), style = "text-align: center;"),
                  div(img(src = "participation.png"), style = "text-align: center;"),
-                 h5("SNAP is an important stimulus during economic recessions, since SNAP
+                 h4("SNAP is an important stimulus during economic recessions, since SNAP
                     recipients are quick to redeem their benefits, thereby boosting local
                     economies. The number of SNAP retailers is at an all-time high--more
                     than a quarter million retailers across the nation as of 2013. SNAP
